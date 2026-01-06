22d38085891745c979136d129b0c802efe9bbe57
kaer_mac <kaer.business999@gmail.com>
2026-01-06 16:47:11 +0800

    11

---
+++ server/service/mainClient/handler.go
@@ -0,0 +1,200 @@
+package mainClient
+
+import (
+	"compoment/ormutil"
+	"compoment/ws"
+	"encoding/json"
+	"errors"
+	"service/comm"
+	"service/mainClient/game"
+	"service/mainClient/game/qznn"
+	"service/modelClient"
+
+	"github.com/gin-gonic/gin"
+)
+
+type LobbyConfigRsp struct {
+	LobbyConfigs interface{} `json:"LobbyConfigs"`
+}
+
+func handlePlayerJoin(connWrap *ws.WsConnWrap, appId, appUserId string, data []byte) error {
+	var req struct {
+		Level      int
+		BankerType int
+	}
+	// 默认进入初级场
+	req.Level = 1
+	req.BankerType = -1 // 默认值，用于检测客户端是否传递
+
+	if err := json.Unmarshal(data, &req); err != nil {
+		return comm.ErrClientParam
+	}
+	if req.Level <= 0 {
+		req.Level = 1
+	}
+
+	cfg := qznn.GetConfig(req.Level)
+	if cfg == nil {
+		return comm.NewMyError("无效的房间类型")
+	}
+
+	// 校验 BankerType 是否合法
+	if req.BankerType != qznn.BankerTypeNoLook &&
+		req.BankerType != qznn.BankerTypeLook3 &&
+		req.BankerType != qznn.BankerTypeLook4 {
+		return comm.NewMyError("无效的抢庄类型")
+	}
+
+	// 检查余额
+	user, err := modelClient.GetOrCreateUser(appId, appUserId)
+	if err != nil {
+		return comm.NewMyError("获取用户信息失败")
+	}
+	if user.Balance < cfg.MinBalance {
+		return comm.NewMyError("余额不足!")
+	}
+
+	userId := appId + appUserId
+	// 处理抢庄牛牛匹配逻辑
+	p := qznn.NewPlayer()
+	p.ID = userId
+	p.ConnWrap = connWrap
+	p.Balance = user.Balance
+	p.NickName = func() string {
+		if user.NickName == "" {
+			return user.UserId
+		}
+		return user.NickName
+	}()
+
+	room, err := game.GetMgr().JoinOrCreateNNRoom(p, req.Level, req.BankerType)
+	if err != nil {
+		return err
+	}
+	room.Broadcast(comm.PushData{
+		Cmd:      comm.ServerPush,
+		PushType: qznn.PushPlayJoin,
+		Data: qznn.PushPlayerJoinStruct{
+			Room:   room,
+			UserId: userId,
+		},
+	})
+
+	return err
+}
+
+func handlePlayerLeave(userId string, data []byte) error {
+	var req struct {
+		RoomId string
+	}
+	if err := json.Unmarshal(data, &req); err != nil {
+		return comm.ErrClientParam
+	}
+	room := game.GetMgr().GetRoomByRoomId(req.RoomId)
+	return qznn.HandlerPlayerLeave(room, userId)
+}
+
+func handlePlayerCallBank(userId string, data []byte) error {
+	var req struct {
+		RoomId string
+		Mult   int64
+	}
+	if err := json.Unmarshal(data, &req); err != nil {
+		return comm.ErrClientParam
+	}
+	room := game.GetMgr().GetRoomByRoomId(req.RoomId)
+	return qznn.HandleCallBanker(room, userId, req.Mult)
+}
+
+func handlePlayerPlaceBet(userId string, data []byte) error {
+	var req struct {
+		RoomId string
+		Mult   int64
+	}
+	if err := json.Unmarshal(data, &req); err != nil {
+		return comm.ErrClientParam
+	}
+	room := game.GetMgr().GetRoomByRoomId(req.RoomId)
+	return qznn.HandlePlaceBet(room, userId, req.Mult)
+}
+
+func handlePlayerShowCard(userId string, data []byte) error {
+	var req struct {
+		RoomId string
+	}
+	if err := json.Unmarshal(data, &req); err != nil {
+		return comm.ErrClientParam
+	}
+	room := game.GetMgr().GetRoomByRoomId(req.RoomId)
+	return qznn.HandleShowCards(room, userId)
+}
+
+func handleUserInfo(appId string, appUserId string) (*modelClient.ModelUser, error) {
+	user, err := modelClient.GetOrCreateUser(appId, appUserId)
+	if err != nil {
+		return nil, errors.New("获取用户信息失败")
+	}
+	nickName := user.NickName
+	if nickName == "" {
+		nickName = user.UserId
+	}
+
+	return user, nil
+}
+
+func handleSaveSetting(userId string, data []byte) error {
+	var req struct {
+		Music  bool
+		Effect bool
+		Talk   bool
+	}
+	if err := json.Unmarshal(data, &req); err != nil {
+		return comm.ErrClientParam
+	}
+	_, err := modelClient.UpdateUserParam(userId,
+		ormutil.WithChangerMap(map[string]interface{}{"music": req.Music, "effect": req.Effect,
+			"talk": req.Talk}))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func handlerPlayerTalk(userId string, data []byte) error {
+	var req struct {
+		RoomId string
+		Type   int
+		Index  int
+	}
+	if err := json.Unmarshal(data, &req); err != nil {
+		return comm.ErrClientParam
+	}
+
+	room := game.GetMgr().GetRoomByRoomId(req.RoomId)
+	if room != nil {
+		room.Broadcast(comm.PushData{
+			Cmd:      comm.ServerPush,
+			PushType: qznn.PushTalk,
+			Data: qznn.PushTalkStruct{
+				UserId: userId,
+				Type:   req.Type,
+				Index:  req.Index}})
+	}
+
+	return nil
+}
+
+func handleLobbyConfig() *LobbyConfigRsp {
+	return &LobbyConfigRsp{
+		LobbyConfigs: qznn.Configs,
+	}
+}
+
+func RpcQZNNData(c *gin.Context) {
+	rooms := game.GetMgr().GetAllRooms()
+	c.JSON(200, gin.H{
+		"code": 0,
+		"msg":  "success",
+		"data": rooms,
+	})
+}

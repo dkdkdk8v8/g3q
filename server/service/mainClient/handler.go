0fbd7d0867f14d2fd61937df5b7c8a5792ae968b
kaer_mac <kaer.business999@gmail.com>
2026-01-09 03:59:26 +0800

    111

--- server/service/mainClient/handler.go
+++ server/service/mainClient/handler.go
@@ -2,6 +2,7 @@ package mainClient
 
 import (
 	"compoment/ormutil"
+	"compoment/util"
 	"compoment/ws"
 	"encoding/json"
 	"errors"
@@ -15,6 +16,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 type LobbyConfigRsp struct {
@@ -227,129 +229,129 @@ var handerGameRecordCache = modelComm.WrapCache[*handleGameRecordRsp](handleGame
 	2*time.Second).(func(userId string, data []byte) (*handleGameRecordRsp, error))
 
 func handleGameRecord(userId string, data []byte) (*handleGameRecordRsp, error) {
-	// var req struct {
-	// 	Limit         int
-	// 	LastId        uint64
-	// 	Date          string //20250530
-	// 	LastTimestamp int64
-	// }
-	// if err := json.Unmarshal(data, &req); err != nil {
-	// 	return nil, comm.ErrClientParam
-	// }
-	// if req.Limit > 20 {
-	// 	req.Limit = 20
-	// }
-	// if req.Limit <= 0 {
-	// 	req.Limit = 10
-	// }
-	// var start = time.Unix(0, 0)
-	// var end = time.Now()
+	var req struct {
+		Limit         int
+		LastId        uint64
+		Date          string //20250530
+		LastTimestamp int64
+	}
+	if err := json.Unmarshal(data, &req); err != nil {
+		return nil, comm.ErrClientParam
+	}
+	if req.Limit > 20 {
+		req.Limit = 20
+	}
+	if req.Limit <= 0 {
+		req.Limit = 10
+	}
+	var start = time.Unix(0, 0)
+	var end = time.Now()
 
-	// if req.Date != "" {
-	// 	//main里面已经设置了location
-	// 	t, err := time.ParseInLocation("20060102", req.Date, util.LocShanghai)
-	// 	if err != nil {
-	// 		return nil, comm.ErrClientParam
-	// 	}
-	// 	start = t
-	// 	end = start.Add(24 * time.Hour)
-	// }
+	if req.Date != "" {
+		//main里面已经设置了location
+		t, err := time.ParseInLocation("20060102", req.Date, util.LocShanghai)
+		if err != nil {
+			return nil, comm.ErrClientParam
+		}
+		start = t
+		end = start.Add(24 * time.Hour)
+	}
 
-	// var rsp handleGameRecordRsp
+	var rsp handleGameRecordRsp
 
-	// var lastTime = time.Unix(0, 0)
+	var lastTime = time.Unix(0, 0)
 
-	// var currentSummy *recordSummery
-	// itemCount := 0
-	// loopCount := 0
-	// targetCount := req.Limit
+	var currentSummy *recordSummery
+	itemCount := 0
+	loopCount := 0
+	targetCount := req.Limit
 
-	// for {
-	// 	loopCount++
-	// 	if loopCount > 100 { // 保护逻辑：防止死循环
-	// 		logrus.WithField("!", nil).WithField("userId", userId).Error("loopMax")
-	// 		break
-	// 	}
+	for {
+		loopCount++
+		if loopCount > 100 { // 保护逻辑：防止死循环
+			logrus.WithField("!", nil).WithField("userId", userId).Error("loopMax")
+			break
+		}
 
-	// 	if itemCount >= targetCount {
-	// 		//加快拉取数据的速度
-	// 		req.Limit = req.Limit * 2
-	// 		if req.Limit > 1000 {
-	// 			req.Limit = 1000
-	// 		}
-	// 	}
-	// 	records, err := modelClient.GetUserGameRecordsJoinGameRecord(userId, req.Limit, req.LastId, start, end)
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	if len(records) == 0 {
-	// 		break
-	// 	}
+		if itemCount >= targetCount {
+			//加快拉取数据的速度
+			req.Limit = req.Limit * 2
+			if req.Limit > 1000 {
+				req.Limit = 1000
+			}
+		}
+		records, err := modelClient.GetUserGameRecordsJoinGameRecord(userId, req.Limit, req.LastId, start, end)
+		if err != nil {
+			return nil, err
+		}
+		if len(records) == 0 {
+			break
+		}
 
-	// 	// 更新LastId，防止死循环
-	// 	req.LastId = records[len(records)-1].Id
+		// 更新LastId，防止死循环
+		req.LastId = records[len(records)-1].Id
 
-	// 	for _, userRecoed := range records {
-	// 		if lastTime.IsZero() || !util.IsSameDay(lastTime, userRecoed.CreateAt) {
-	// 			if itemCount >= targetCount {
-	// 				//客户端的条件满足了，并且又跨天了，不在需要统计当日的nSummy
-	// 				return &rsp, nil
-	// 			}
-	// 			currentSummy = &recordSummery{
-	// 				Type: 0,
-	// 				Date: userRecoed.CreateAt.Format("01月02") + "周" +
-	// 					GetChineseWeekName(int(userRecoed.CreateAt.Weekday())),
-	// 			}
-	// 			rsp.List = append(rsp.List, currentSummy)
-	// 		}
-	// 		lastTime = userRecoed.CreateAt
+		for _, userRecoed := range records {
+			if lastTime.IsZero() || !util.IsSameDay(lastTime, userRecoed.CreateAt) {
+				if itemCount >= targetCount {
+					//客户端的条件满足了，并且又跨天了，不在需要统计当日的nSummy
+					return &rsp, nil
+				}
+				currentSummy = &recordSummery{
+					Type: 0,
+					Date: userRecoed.CreateAt.Format("01月02") + "周" +
+						GetChineseWeekName(int(userRecoed.CreateAt.Weekday())),
+				}
+				rsp.List = append(rsp.List, currentSummy)
+			}
+			lastTime = userRecoed.CreateAt
 
-	// 		// gameRecord, err := modelClient.GetGameRecordByIdCache(userRecoed.GameRecordId)
-	// 		// if err != nil {
-	// 		// 	logrus.WithField("!", nil).WithField("userId", userId).WithError(err).Error("GetGameRecordByIdCache-Fail")
-	// 		// 	continue
-	// 		// }
-	// 		nRecord := &recordItem{
-	// 			Type:          1,
-	// 			BalanceBefore: userRecoed.BalanceBefore,
-	// 			BalanceAfter:  userRecoed.BalanceAfter,
-	// 			GameName:      userRecoed.GameName,
-	// 			CreateAt:      userRecoed.CreateAt}
+			// gameRecord, err := modelClient.GetGameRecordByIdCache(userRecoed.GameRecordId)
+			// if err != nil {
+			// 	logrus.WithField("!", nil).WithField("userId", userId).WithError(err).Error("GetGameRecordByIdCache-Fail")
+			// 	continue
+			// }
+			nRecord := &recordItem{
+				Type:          1,
+				BalanceBefore: userRecoed.BalanceBefore,
+				BalanceAfter:  userRecoed.BalanceAfter,
+				GameName:      userRecoed.GameName,
+				CreateAt:      userRecoed.CreateAt}
 
-	// 		if currentSummy != nil {
-	// 			// 优化：统一计算输赢
-	// 			currentSummy.TotalWinBalance += (userRecoed.BalanceAfter - userRecoed.BalanceBefore)
+			if currentSummy != nil {
+				// 优化：统一计算输赢
+				currentSummy.TotalWinBalance += (userRecoed.BalanceAfter - userRecoed.BalanceBefore)
 
-	// 			switch userRecoed.GameName {
-	// 			case qznn.GameName:
-	// 				var qznnRoom qznn.QZNNRoom
-	// 				err = json.Unmarshal([]byte(userRecoed.GameData), &qznnRoom)
-	// 				if err != nil {
-	// 					continue
-	// 				}
-	// 				for _, player := range qznnRoom.Players {
-	// 					if player.ID == userId {
-	// 						currentSummy.TotalBet += player.ValidBet
-	// 					}
-	// 				}
-	// 				nRecord.GameData = userRecoed.GameData
-	// 			default:
-	// 				// 其他游戏逻辑
-	// 			}
-	// 		}
+				switch userRecoed.GameName {
+				case qznn.GameName:
+					var qznnRoom qznn.QZNNRoom
+					err = json.Unmarshal([]byte(userRecoed.GameData), &qznnRoom)
+					if err != nil {
+						continue
+					}
+					for _, player := range qznnRoom.Players {
+						if player.ID == userId {
+							currentSummy.TotalBet += player.ValidBet
+						}
+					}
+					nRecord.GameData = userRecoed.GameData
+				default:
+					// 其他游戏逻辑
+				}
+			}
 
-	// 		if itemCount < targetCount {
-	// 			//满足客户端ui展示的数据，放入返回数据内
-	// 			rsp.List = append(rsp.List, nRecord)
-	// 			//客户端透传数据，方便下次请求的时候直接准确算偏移量
-	// 			rsp.LastId = userRecoed.Id
-	// 			rsp.LastTimestamp = userRecoed.CreateAt.Unix()
-	// 			itemCount++
-	// 		}
-	// 	}
-	// }
+			if itemCount < targetCount {
+				//满足客户端ui展示的数据，放入返回数据内
+				rsp.List = append(rsp.List, nRecord)
+				//客户端透传数据，方便下次请求的时候直接准确算偏移量
+				rsp.LastId = userRecoed.Id
+				rsp.LastTimestamp = userRecoed.CreateAt.Unix()
+				itemCount++
+			}
+		}
+	}
 
-	// return &rsp, nil
+	return &rsp, nil
 }
 
 type PingRsp struct {

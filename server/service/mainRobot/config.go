5154a3a7bab61dc1cad9bd7fbab7b6b28f33d154
admin <justomen654321@gmail.com>
2026-01-14 19:16:18 +0800

    update robot

--- server/service/mainRobot/config.go
+++ server/service/mainRobot/config.go
@@ -9,14 +9,15 @@ const PATH_WS = "/rpc/ws"
 const PATH_RPC_DATA = "/rpc/qznn-data"
 
 const MIN_ROBOT_ROOMS = 0           // 纯机器人房间最少保持数量
-const MANAGER_LOOP_INTERVAL = 4     // 机器人管理循环间隔(秒)
+const MANAGER_LOOP_INTERVAL = 10    // 机器人管理循环间隔(秒)
 const MIN_GAMES = 20                // 机器人至少玩几局
-const PROB_LEAVE_5_PLAYERS = 0.7    // 5人时退出概率
-const PROB_LEAVE_4_PLAYERS = 0.5    // 4人时退出概率
-const PROB_LEAVE_3_PLAYERS = 0.2    // 3人时退出概率
+const PROB_LEAVE_5_PLAYERS = 0.2    // 5人时退出概率
+const PROB_LEAVE_4_PLAYERS = 0.15   // 4人时退出概率
+const PROB_LEAVE_3_PLAYERS = 0.1    // 3人时退出概率
 const PROB_LEAVE_2_PLAYERS = 0.0    // 2人时退出概率
 const ROBOT_BALANCE_MULT_MIN = 10.0 // 机器人进入房间携带金额最小倍数
 const ROBOT_BALANCE_MULT_MAX = 30.0 // 机器人进入房间携带金额最大倍数
+const ROOM_LEAVE_COOLDOWN = 60      //  房间退出冷却时间(秒)
 
 // 机器人允许进入的房间等级配置
 var ALLOWED_LEVELS = []int{1, 2, 3, 4, 5, 6}

8f5bf9389b0ffb77b4838149eb355e6e7d3635ba
厉害 <14605659+wanwuapp@user.noreply.gitee.com>
2026-01-16 02:59:07 +0800

    update logic

--- client/components.d.ts
+++ client/components.d.ts
@@ -19,11 +19,5 @@ declare module 'vue' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SettingsModal: typeof import('./src/components/SettingsModal.vue')['default']
-    VanDatePicker: typeof import('vant/es')['DatePicker']
-    VanIcon: typeof import('vant/es')['Icon']
-    VanImage: typeof import('vant/es')['Image']
-    VanLoading: typeof import('vant/es')['Loading']
-    VanPopup: typeof import('vant/es')['Popup']
-    VanSwitch: typeof import('vant/es')['Switch']
   }
 }

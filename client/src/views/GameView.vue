8522e92b81fb5bd910a2ce6e7400c8ad006db463
厉害 <14605659+wanwuapp@user.noreply.gitee.com>
2026-01-15 23:57:01 +0800

    update logic

--- client/src/views/GameView.vue
+++ client/src/views/GameView.vue
@@ -1288,7 +1288,7 @@ watch(() => myPlayer.value && myPlayer.value.isShowHand, (val) => {
                             <div v-if="shouldShowRobMult" class="status-content">
                                 <span v-if="myPlayer.robMultiplier > 0" class="status-text rob-text text-large">抢{{
                                     myPlayer.robMultiplier
-                                }}倍</span>
+                                    }}倍</span>
                                 <span v-else class="status-text no-rob-text text-large">不抢</span>
                             </div>
                         </Transition>
@@ -2195,7 +2195,7 @@ watch(() => myPlayer.value && myPlayer.value.isShowHand, (val) => {
 }
 
 .my-hand-cards-area .hand-type-img {
-    height: 50px;
+    height: 45px;
     /* Doubled size */
     object-fit: contain;
     vertical-align: middle;

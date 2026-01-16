486f7b399604aced6cae41ea535b7137fc575e73
厉害 <14605659+wanwuapp@user.noreply.gitee.com>
2026-01-16 17:18:32 +0800

    update logic

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,10 @@ import path from 'path' // Need to import path
 
 export default defineConfig(({ mode }) => {
   return {
+    server: {
+      host: '172.20.10.4',
+      port: 5173,
+    },
     plugins: [
       vue(),
       AutoImport({

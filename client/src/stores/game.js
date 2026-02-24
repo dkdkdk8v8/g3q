45f77539c408f896998dd8a918324c1700ab56f3
厉害 <14605659+wanwuapp@user.noreply.gitee.com>
2026-02-24 19:49:10 +0800

    update logic

--- client/src/stores/game.js
+++ client/src/stores/game.js
@@ -244,23 +244,23 @@ export const useGameStore = defineStore('game', () => {
                 handResult = { type: res.type, typeName: res.typeName, multiplier: res.multiplier, bullIndices: res.bullIndices };
 
                 // --- DEBUG: FORCE NIU FOR EVERYONE ---
-                if (p.ID !== myPlayerId.value) {
-                    const debugOptions = [
-                        { type: 'BULL_6', typeName: '牛6' },
-                        { type: 'BULL_7', typeName: '牛7' },
-                        { type: 'BULL_8', typeName: '牛8' },
-                        { type: 'BULL_9', typeName: '牛9' },
-                    ];
-                    // Pick based on seat num to be deterministic per seat but different across seats
-                    const debugChoice = debugOptions[(p.SeatNum || 0) % debugOptions.length];
-                    
-                    handResult.type = debugChoice.type;
-                    handResult.typeName = debugChoice.typeName;
-                } else {
-                     // Force My Player to Niu Niu
-                     handResult.type = 'BULL_BULL';
-                     handResult.typeName = '牛牛';
-                }
+                // if (p.ID !== myPlayerId.value) {
+                //     const debugOptions = [
+                //         { type: 'BULL_6', typeName: '牛6' },
+                //         { type: 'BULL_7', typeName: '牛7' },
+                //         { type: 'BULL_8', typeName: '牛8' },
+                //         { type: 'BULL_9', typeName: '牛9' },
+                //     ];
+                //     // Pick based on seat num to be deterministic per seat but different across seats
+                //     const debugChoice = debugOptions[(p.SeatNum || 0) % debugOptions.length];
+
+                //     handResult.type = debugChoice.type;
+                //     handResult.typeName = debugChoice.typeName;
+                // } else {
+                //      // Force My Player to Niu Niu
+                //      handResult.type = 'BULL_BULL';
+                //      handResult.typeName = '牛牛';
+                // }
                 // ----------------------------------------------------
             }
 
